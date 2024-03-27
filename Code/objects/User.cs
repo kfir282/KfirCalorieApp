84fd7e35704fbb6d2a787ee4995de47dde7c2e87
Amit G <amitginat15@gmail.com>
2024-03-27 18:45:07 +0200

    Pushes

--- Code/objects/User.cs
+++ Code/objects/User.cs
@@ -16,21 +16,23 @@ namespace KfirCalorieCounterReal.objects
         private string name;
         private string email;
         private string password;
+        private int calorieGoal;
         private Day currentDay;
         private List<Day> saved; 
 
-        public User(string name, string email, string password)
+        public User(string name, string email, string password, int calorieGoal)
         {
             this.name = name;
             this.email = email;
             this.password = password;
             this.currentDay = new Day();
             this.saved = new List<Day>();
+            this.calorieGoal = calorieGoal;
         }
 
         public string GetPassword() {  return this.password; }
         public string GetName() { return this.name; }
         public string GetEmail() { return this.email; }
-
+        public int GetCalorieGoal() {  return this.calorieGoal; }
     }
 }
